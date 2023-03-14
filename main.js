04e39a37e66de9b9d32f782739ea9784be8eab3f
Michelle Totherow <tym4me13@gmail.com>
2023-03-14 16:39:11 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,101 @@
+// const quarterValue = (newValue) => {
+//     const quarter = newValue / 4
+
+// return quarter
+// }
+
+// // Invoke the function and store the return value
+// const newNumber = quarterValue(200)
+
+
+// // Log the return value
+// console.log(newNumber)
+
+
+// steps for walking dog
+// check weather
+// get jacket
+// put leash on dog 
+// open door
+// walk dog
+
+
+
+const checkWeather= () => {    
+console.log("Pretty Day")
+}
+
+const getJacket = () => {
+ console.log("Jacket is on.")
+}
+
+const leashOn= () => {
+console.log("Leash is on pup.")
+}
+
+const openDoor= () => {
+ console.log("Open the door.")
+}
+
+const walkDog= () => {
+    console.log("Walking the puppy.")
+}
+
+checkWeather(1)
+getJacket(2)
+leashOn(3)
+openDoor(4)
+walkDog(5)
+
+
+
+
+const rotateCake = () => {
+	// Code to rotate the cake
+    console.log("Rotate.")
+}
+
+const gatherIngredients = () => {
+	// Code to gather the ingredients from storage
+    console.log("Gather Ingredients.")
+}
+
+const measureIngredients = () => {
+	// Code to measure the ingredients
+    console.log("Measure the ingredients.")
+}
+
+const addFrosting = () => {
+	// Code to add frosting to a baked cake
+    console.log("Add frosting,")
+}
+
+const startOven = () => {
+	// Code to turn on the oven to a specific temperature
+    console.log("Turn on the oven.")
+}
+
+const createBatter = () => {
+	// Code to create some cake batter
+    console.log("Create the cake batter.")
+}
+
+const checkIfDone = () => {
+	// Code to check if the cake is done
+    console.log("Check cake.")
+
+}
+
+const startBaking = () => {
+	// Code to put the cake batter into the oven
+    console.log("Put cake in the oven.")
+}
+
+startOven()
+gatherIngredients()
+measureIngredients()
+createBatter()
+startBaking()
+rotateCake()
+checkIfDone()
+addFrosting()
